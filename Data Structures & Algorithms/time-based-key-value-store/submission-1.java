c0b6c7905cb6ce64fa357051e44629c1db4777ec
Navneet Vishwakarma <navneet.nifft@gmail.com>
2026-04-30 20:58:35 +0530

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.java
@@ -0,0 +1,38 @@
+class TimeMap {
+
+    public record ValueRecord(String value, int timestamp) {};
+    Map<String, List<ValueRecord>> map;
+
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        ValueRecord val = new ValueRecord(value, timestamp);
+        List<ValueRecord> values = map.getOrDefault(key, new ArrayList<>());
+        values.add(val);
+        map.put(key, values);
+    }
+    
+    public String get(String key, int timestamp) {
+        List<ValueRecord> values = map.get(key);
+        return find(values, timestamp);
+    }
+
+    private String find(List<ValueRecord> values, int timestamp) {
+        String result = "";
+        if (values == null) return result;
+        int l = 0, r = values.size() - 1;
+        while (l <= r) {
+            int m = (l+r)/2;
+            ValueRecord val = values.get(m);
+            if (timestamp < val.timestamp) {
+                r = m-1;
+            } else {
+                result = val.value;
+                l = m+1;
+            }
+        }
+        return result;
+    }
+}
